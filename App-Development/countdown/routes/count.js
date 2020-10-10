16b5283d1e12443a1d53bd211656b38e351c2f19
livrbecca <69110329+livrbecca@users.noreply.github.com>
2020-10-10 18:28:36 +0100

    fixed folder issues git commit -m'fixed folder issue' i

--- App-Development/countdown/routes/count.js
+++ App-Development/countdown/routes/count.js
@@ -1,18 +1,26 @@
 var express = require("express");
 var router = express.Router();
 
-let launchTime = new Date("12/29/2020 10:00:54").getTime();
-
+let launchTime = new Date(2021, 12, 11, 18, 0, 54);
+let now;
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  const currentTime = new Date().getTime();
+  const currentTime = new Date();
+  const numberOfSeconds = (launchTime - currentTime.getDate()) / 1000;
+  const days = Math.floor(numberOfSeconds / (60 * 60 * 24));
+  console.log(numberOfSeconds);
+  console.log(currentTime.toString());
+  console.log("end date + " + launchTime.toString());
 
-  const numberOfSeconds = parseInt(launchTime - currentTime);
+  setInterval(function () {
+    console.log(new Date().toString());
+    launchTime = new Date().toString();
+  }, 1000);
 
   res.render("count", {
     title: "My Awesome Game",
-    countDown: numberOfSeconds,
+    countDown: launchTime,
   });
 });
 
